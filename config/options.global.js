1a58e881e41fd1a929caf681cc217140ac5fb586
binlagin <bengraham11@gmail.com>
2018-01-22 20:48:59 -0500

    Additions to global config

--- config/options.global.js
+++ config/options.global.js
@@ -1,6 +1,10 @@
 module.exports = {
+	//This is the name of the application you created inside the Honeywell Developer's portal
 	appName: 'home-automation',
+	//This is the URL the API will post back too when completed
 	appAuthorizeUri: 'http://localhost:3000/auth',
+	//Consumer Key - Get this from your application view inside the Honeywell Developer's portal
 	consumerKey: '',
+	//Consumer Secret - Get this from your application view inside the Honeywell Developer's portal
 	consumerPrivate: ''
 };
